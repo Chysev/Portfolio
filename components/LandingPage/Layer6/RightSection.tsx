30f091614d3d9f8be17bb4edac825ed97d239ef1
Chysev <Johnlayda92@gmail.com>
2022-11-13 00:11:23 +0400

    Portfolio

--- components/LandingPage/Layer6/RightSection.tsx
+++ components/LandingPage/Layer6/RightSection.tsx
@@ -1,17 +1,47 @@
+import Slider from 'react-slick'
+
 // Components
 import FacebookCard from "../../Cards/FacebookCard"
 import DiscordCard from "../../Cards/DiscordCard"
 import GitHubCard from "../../Cards/GitHubCard"
 import InstagramCard from "../../Cards/InstagramCard"
 
-const RightSection = () => {
+const RightSection = (props) => {
+
+    const settings = {
+        className: "max-w-[400px] 480max:max-w-[320px] 380max:max-w-[240px] mr-[20px] 992max:mr-[0]",
+        dots: true,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 2000,
+        infinite: true,
+        swipeToSlide: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+    };
     return (
-        <div className="grid grid-cols-2 992max:grid-cols-1">
-            <FacebookCard />
-            <DiscordCard />
-            <GitHubCard />
-            <InstagramCard />
+        <div>
+            <Slider {...settings} >
+                <div>
+                    <FacebookCard />
+                </div>
+
+                <div>
+                    <DiscordCard />
+                </div>
+
+                <div>
+                    <GitHubCard />
+                </div>
+
+                <div>
+                    <InstagramCard />
+                </div>
+            </Slider>
         </div>
+
+
     )
 }
 
