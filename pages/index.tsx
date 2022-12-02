78c0b0b8fee2cc7daf360f87073bec410ce61359
Chysev <Johnlayda92@gmail.com>
2022-12-02 12:19:49 +0400

    Portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import dynamic from "next/dynamic"
-
 // Component
-const Layer1 = dynamic(() => import("../components/LandingPage/Layer1"), { ssr: false })
-const Layer2 = dynamic(() => import("../components/LandingPage/Layer2"), { ssr: false })
-const Layer3 = dynamic(() => import("../components/LandingPage/Layer3"), { ssr: false })
-const Layer4 = dynamic(() => import("../components/LandingPage/Layer4"), { ssr: false })
-const Layer5 = dynamic(() => import("../components/LandingPage/Layer5"), { ssr: false })
-const Layer6 = dynamic(() => import("../components/LandingPage/Layer6"), { ssr: false })
-const Layer7 = dynamic(() => import("../components/LandingPage/Layer7"), { ssr: false })
-
+import Layer1 from "../components/LandingPage/Layer1"
+import Layer2 from "../components/LandingPage/Layer2"
+import Layer3 from "../components/LandingPage/Layer3"
+import Layer4 from "../components/LandingPage/Layer4"
+import Layer5 from "../components/LandingPage/Layer5"
+import Layer6 from "../components/LandingPage/Layer6"
+import Layer7 from "../components/LandingPage/Layer7"
 
 const Home = () => {
 
