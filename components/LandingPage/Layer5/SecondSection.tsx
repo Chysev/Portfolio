ad7d5e690bbce90d113a2b034b4f314a1f1aa817
Chysev <Johnlayda92@gmail.com>
2022-12-27 00:06:55 +0400

    Portfolio

--- components/LandingPage/Layer5/SecondSection.tsx
+++ components/LandingPage/Layer5/SecondSection.tsx
@@ -1,10 +1,12 @@
 // Component
 import SchoolProjectCard from '../../Cards/SchoolProjectCard'
+import CapybaraCard from '../../Cards/CapybaraCard'
 
 const SecondSection = () => {
     return (
         <div className="grid  max-w-[1300px] grid-cols-3 items-center gap-10 992max:flex 992max:max-w-[520px] 992max:flex-col 380max:max-w-[370px] 1360max:grid-cols-2">
             <SchoolProjectCard />
+            <CapybaraCard />
         </div>
     )
 }
