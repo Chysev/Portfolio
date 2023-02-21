31648329adb8e9d0975e9129d42114c8ed93a3b4
Chysev <Johnlayda92@gmail.com>
2023-02-21 21:53:47 +0400

    Portfolio

--- components/LandingPage/Layer2/LeftSection.tsx
+++ components/LandingPage/Layer2/LeftSection.tsx
@@ -3,14 +3,18 @@ import MERN from "../../MiniCards/Mern"
 import MEVN from "../../MiniCards/Mevn"
 import MEAN from "../../MiniCards/Mean"
 import EJS from "../../MiniCards/Ejs"
+import Discord from "../../MiniCards/Discord"
 
 const LeftSection = () => {
     return (
         <div className="grid grid-cols-2 992max:grid-cols-1">
             <MERN />
             <MEVN />
-            <MEAN />
-            <EJS />
+            <Discord />
+            <div className="grid">
+                <MEAN />
+                <EJS />
+            </div>
         </div>
     )
 }
--- components/MiniCards/Mern.tsx
+++ components/MiniCards/Mern.tsx
@@ -2,7 +2,7 @@ const Mern = () => {
     return (
         <div className="rounded-[10px_0px_10px_10px] flex leading-[24px] items-center  my-[0.8rem] mx-[0.5rem] gap-[1rem] bg-lightsurface py-[35px] px-[50px] dark:bg-darksurface 768max:py-[30px] 992max:px-[100px] 640max:py-[30px] 640max:px-[80px] 380max:py-[20px] 380max:px-[20px]">
             <i className="text-[50px] text-darkcolor dark:text-white fa-brands fa-react" />
-            <h1 className="text-[16px] flex leading-6 text-lightcolor dark:text-white">MERN Stack</h1>
+            <h1 className="text-[16px] flex leading-6 text-lightcolor dark:text-[#fff]">MERN Stack</h1>
         </div>
     );
 };
